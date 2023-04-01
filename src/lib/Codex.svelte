cb0fcddb2219c677164934bf761210bd817a7e23
Evgeniy Amiraslanov <the.modifikator@gmail.com>
2023-04-01 13:37:41 +0500

    Refactor pages a bit

--- src/lib/Codex.svelte
+++ src/lib/Codex.svelte
@@ -1,19 +1,23 @@
-<script>
+<script lang="ts">
   // source of the concept: https://codepen.io/diemoritat/pen/LKROYZ
-  let pageNo = 1;
-
-  function forward() {
-      pageNo++;
-      if (pageNo > 2) {
-          pageNo = 2;
-      }
+  export enum Page {
+      MIDDLE,
+      BACK,
+      FORWARD,
   }
 
-  function back() {
-      pageNo--;
-      if (pageNo < 0) {
-          pageNo = 0;
-      }
+  let pageNo = Page.MIDDLE;
+
+  function toMiddle() {
+      pageNo = Page.MIDDLE;
+  }
+
+  function toBack() {
+      pageNo = Page.BACK;
+  }
+
+  function toForward() {
+      pageNo = Page.FORWARD;
   }
 </script>
 
@@ -41,10 +45,10 @@
     height: 100%;
     display: flex;
     perspective: 1200px;
+    position: relative;
 
     &__page {
       box-sizing: border-box;
-      position: relative;
       width: 50%;
       height: 100%;
       display: grid;
@@ -58,19 +62,11 @@
         background-image: linear-gradient(-90deg, rgba(227,227,227,1) 0%, rgba(247,247,247,0) 18%);
       }
 
-      &--first {
-        cursor: pointer;
-      }
-
-      &--last {
-        cursor: pointer;
-      }
-
       &--middle-2,
       &--middle-3 {
+        cursor: pointer;
         position: absolute;
         right: 0;
-        pointer-events: none;
         transform-style: preserve-3d;
         background-color: var(--page-bg);
         background-image: linear-gradient(90deg, rgba(227,227,227,1) 0%, rgba(247,247,247,0) 18%);
@@ -114,34 +110,31 @@
 </style>
 
 <div class="cover">
-  <div class="codex" class:codex--back={pageNo === 0} class:codex--forward={pageNo === 2}>
-    <div class="codex__page codex__page--first">
+  <div class="codex" class:codex--back={pageNo === Page.BACK} class:codex--forward={pageNo === Page.FORWARD}>
+    <div class="codex__page codex__page--first" on:click={misclick}>
       Content 1
     </div>
 
-    <div class="codex__page codex__page--last">
+    <div class="codex__page codex__page--last" on:click={misclick}>
       Content 6
     </div>
 
     <div class="codex__page codex__page--middle-2">
-      <div class="codex__page-front">
+      <div class="codex__page-front" on:click={toMiddle}>
         Content 2
       </div>
-      <div class="codex__page-back">
+      <div class="codex__page-back" on:click={toBack}>
         Content 3
       </div>
     </div>
 
     <div class="codex__page codex__page--middle-3">
-      <div class="codex__page-front">
+      <div class="codex__page-front" on:click={toForward}>
         Content 4
       </div>
-      <div class="codex__page-back">
+      <div class="codex__page-back" on:click={toMiddle}>
         Content 5
       </div>
     </div>
   </div>
 </div>
-
-<button on:click={back}>Back</button>
-<button on:click={forward}>Forward</button>
