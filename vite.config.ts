3d9724959d9ad4e46a803a8b2dd0ece3614006ea
Evgeniy Amiraslanov <the.modifikator@gmail.com>
2023-04-06 13:15:49 +0500

    Add source maps

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [svelte(), createHtmlPlugin({
     inject: {
       data: {
